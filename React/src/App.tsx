92ac9333e9ddb7448ad3110ff80984f87d3e7dc1
Diana Gromova <16adianay@gmail.com>
2025-07-11 20:51:25 +0400

    React example

--- React/src/App.tsx
+++ React/src/App.tsx
@@ -1,16 +1,11 @@
-import { useCallback, useState } from 'react';
+import ChatApp from './components/ChatApp.tsx';
 import './App.css';
-import 'devextreme/dist/css/dx.material.blue.light.compact.css';
-import Button from 'devextreme-react/button';
+import 'devextreme/dist/css/dx.fluent.blue.light.css';
 
 function App(): JSX.Element {
-  var [count, setCount] = useState<number>(0);
-  const clickHandler = useCallback(() => {
-    setCount((prev) => prev + 1);
-  }, [setCount]);
   return (
     <div className="main">
-      <Button text={`Click count: ${count}`} onClick={clickHandler} />
+      <ChatApp />
     </div>
   );
 }
