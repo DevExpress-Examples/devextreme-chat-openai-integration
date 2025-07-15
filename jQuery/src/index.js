82ad64ea78ebfa3b223297d5c01621b82a54ee70
Diana Gromova <16adianay@gmail.com>
2025-07-15 18:22:32 +0400

    add data

--- jQuery/src/index.js
+++ jQuery/src/index.js
@@ -6,23 +6,10 @@ import { unified } from 'unified';
 import remarkParse from 'remark-parse';
 import remarkRehype from 'remark-rehype';
 import rehypeStringify from 'rehype-stringify';
-
-/** OpenAI integration */
-const deployment = 'gpt-4o-mini';
-const apiKey = 'OPENAI_API_KEY';
-/** End */
-const REGENERATION_TEXT = 'Regeneration...';
-const CHAT_MESSAGEBOX_BUTTON_CLASS = 'dx-chat-messagebox-button';
-const CHAT_MESSAGEBOX_TEXTAREA_CLASS = 'dx-chat-messagebox-textarea';
-
-const user = {
-  id: 'user',
-};
-
-const assistant = {
-  id: 'assistant',
-  name: 'Virtual Assistant',
-};
+import {
+  assistant, deployment, user, apiKey,
+  CHAT_MESSAGEBOX_BUTTON_CLASS, CHAT_MESSAGEBOX_TEXTAREA_CLASS, REGENERATION_TEXT,
+} from './data';
 
 $(() => {
   const store = [];
