09da7e52e8cb07b807bc387032c4f30d6275ece4
Diana Gromova <16adianay@gmail.com>
2025-07-15 17:33:38 +0400

    Update .eslintrc.js

--- jQuery/.eslintrc.js
+++ jQuery/.eslintrc.js
@@ -1,7 +1,7 @@
 module.exports = {
   root: true,
   rules: {
-    'spellcheck/spell-checker': ['off'],
+    'spellcheck/spell-checker': 'off',
   },
   extends: ['devextreme/spell-check'],
   overrides: [{
