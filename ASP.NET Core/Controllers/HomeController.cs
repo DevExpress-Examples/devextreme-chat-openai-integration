21d1b6e2e524ab274e792eb905777541d4f3802e
Diana Gromova <16adianay@gmail.com>
2025-07-15 18:43:26 +0400

    ASP.NET Core example

--- ASP.NET Core/Controllers/HomeController.cs
+++ ASP.NET Core/Controllers/HomeController.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using ASP_NET_Core.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace ASP_NET_Core.Controllers
@@ -10,7 +11,10 @@ namespace ASP_NET_Core.Controllers
     {
         public IActionResult Index()
         {
-            return View();
+            return View(new ChatViewModel
+            {
+                CurrentUser = new ChatUser { Id = "user" }
+            });
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
--- ASP.NET Core/Models/SampleOrder.cs
+++ ASP.NET Core/Models/SampleOrder.cs
@@ -3,14 +3,44 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Text.Json.Serialization;
 
 namespace ASP_NET_Core.Models {
-    public class SampleOrder {
-        public int OrderID { get; set; }
-        public DateTime OrderDate { get; set; }
-        public string CustomerID { get; set; }
-        public string CustomerName { get; set; }
-        public string ShipCountry { get; set; }
-        public string ShipCity { get; set; }
+    public class ChatUser
+    {
+        [JsonPropertyName("id")]
+        public string Id { get; set; }
+
+        [JsonPropertyName("name")]
+        public string Name { get; set; }
+
+        [JsonPropertyName("avatarUrl")]
+        public string AvatarUrl { get; set; }
+    }
+    public class Message
+    {
+        [JsonPropertyName("id")]
+        public string Id { get; set; }
+
+        [JsonPropertyName("timestamp")]
+        public DateTime Timestamp { get; set; }
+
+        [JsonPropertyName("author")]
+        public ChatUser Author { get; set; }
+
+        [JsonPropertyName("text")]
+        public string Text { get; set; }
+
+        [JsonPropertyName("isDeleted")]
+        public Boolean IsDeleted { get; set; }
+
+        [JsonPropertyName("isEdited")]
+        public Boolean IsEdited { get; set; }
+    }
+    public class ChatViewModel
+    {
+        public IEnumerable<Message> Messages { get; set; }
+        public ChatUser CurrentUser { get; set; }
+        public ChatUser SupportAgent { get; set; }
     }
 }
