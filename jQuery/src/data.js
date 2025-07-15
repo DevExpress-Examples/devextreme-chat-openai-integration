33d3b2a5fe6b676f94fd1feeeac7a7d8b4b692c1
Diana Gromova <16adianay@gmail.com>
2025-07-15 18:26:03 +0400

    fix lint

--- jQuery/src/data.js
+++ jQuery/src/data.js
@@ -1,6 +1,5 @@
 export const deployment = 'gpt-4o-mini';
 export const apiKey = 'OPENAI_API_KEY';
-
 export const REGENERATION_TEXT = 'Regeneration...';
 export const CHAT_MESSAGEBOX_BUTTON_CLASS = 'dx-chat-messagebox-button';
 export const CHAT_MESSAGEBOX_TEXTAREA_CLASS = 'dx-chat-messagebox-textarea';
