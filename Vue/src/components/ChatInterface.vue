338188227d740209c06f961f644768b46795cfb7
Diana Gromova <16adianay@gmail.com>
2025-07-15 18:51:01 +0400

    Update ChatInterface.vue

--- Vue/src/components/ChatInterface.vue
+++ Vue/src/components/ChatInterface.vue
@@ -41,7 +41,7 @@
   </div>
 </template>
 
-<script setup>
+<script setup lang="ts">
 import { onMounted } from 'vue';
 import { DxChat } from 'devextreme-vue/chat';
 import { DxButton } from 'devextreme-vue/button';
