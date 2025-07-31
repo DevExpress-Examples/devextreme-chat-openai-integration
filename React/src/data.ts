bda49062ec84771b48703d093e59d2ebb44cf32b
Diana Gromova <16adianay@gmail.com>
2025-07-31 22:18:45 +0400

    Update data.ts

--- React/src/data.ts
+++ React/src/data.ts
@@ -1,9 +1,9 @@
 import { type ChatTypes } from 'devextreme-react/chat';
 
 export const OpenAIConfig = {
-    dangerouslyAllowBrowser: true,
-    apiKey: 'OPEN_AI_KEY',
-    deployment: 'gpt-4o-mini',
+  dangerouslyAllowBrowser: true,
+  apiKey: 'OPEN_AI_KEY',
+  deployment: 'gpt-4o-mini',
 };
 
 export const ALERT_TIMEOUT = 10000;
@@ -11,10 +11,10 @@ export const ALERT_TIMEOUT = 10000;
 export const CHAT_DISABLED_CLASS = 'chat-disabled';
 
 export const user: ChatTypes.User = {
-    id: 'user',
+  id: 'user',
 };
 
 export const assistant: ChatTypes.User = {
-    id: 'assistant',
-    name: 'Virtual Assistant',
+  id: 'assistant',
+  name: 'Virtual Assistant',
 };
